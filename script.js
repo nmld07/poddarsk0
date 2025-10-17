fe44ed97ae5dc6e3bb87798a74a445a877e16049
nmld07 <tuktarovbilal881@gmail.com>
2025-10-17 21:07:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -471,3 +471,12 @@ window.addEventListener('beforeinstallprompt', (e) => {
     deferredPrompt = e;
     showInstallPrompt();
 });
+// Бургер-меню
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav-links');
+
+if (burger && navLinks) {
+    burger.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+}
